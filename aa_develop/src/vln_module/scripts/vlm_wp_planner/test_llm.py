6f10f0b025b7266f50cb75acd94497a0b3ca27f8
J1dan <e1010693@u.nus.edu>
2025-07-15 19:36:12 +0800

    .

--- aa_develop/src/vln_module/scripts/vlm_wp_planner/test_llm.py
+++ aa_develop/src/vln_module/scripts/vlm_wp_planner/test_llm.py
@@ -1,6 +1,11 @@
+import os
 from google import genai
 
-client = genai.Client(api_key="AIzaSyDo6m4aQtQSU7ILS6ZlDxXz6P_YCxmOeoQ")
+api_key = os.getenv("GOOGLE_API_KEY")
+if not api_key:
+    raise ValueError("GOOGLE_API_KEY environment variable not set")
+
+client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
     model="gemini-2.5-flash", contents="Explain how AI works in a few words"
